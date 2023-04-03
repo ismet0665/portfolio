35cc071717842ac4b5520dd5668f346b9b4be641
ismet0665 <ismet.akin@gmail.com>
2023-04-03 22:34:29 +0300

    scrollreveal

--- script.js
+++ script.js
@@ -15,4 +15,15 @@ var typed = new Typed(".profession_text", {
 let date = document.getElementById("date");
 date.innerText = new Date().getFullYear();
 
-ScrollReveal().reveal(".scroll", { delay: 1000 });
+ScrollReveal().reveal(".scroll1", { delay: 250 });
+ScrollReveal().reveal(".scroll2", { delay: 500 });
+ScrollReveal().reveal(".scroll3", { delay: 1000 });
+ScrollReveal().reveal(".scroll4", { delay: 1250 });
+ScrollReveal().reveal(".scroll5", { delay: 1500 });
+ScrollReveal().reveal(".scroll6", { delay: 2000 });
+ScrollReveal().reveal(".scroll7", { delay: 2250 });
+ScrollReveal().reveal(".scroll8", { delay: 2500 });
+ScrollReveal().reveal(".scroll9", { delay: 2750 });
+ScrollReveal().reveal(".scroll10", { delay: 3000 });
+ScrollReveal().reveal(".scroll11", { delay: 3250 });
+ScrollReveal().reveal(".scroll12", { delay: 3500 });
